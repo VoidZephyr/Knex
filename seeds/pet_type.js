59f9f174faaa5c6b43af6a78d09f6dc5d6a37b36
VoidZephyr <jking21412@gmail.com>
2024-11-18 14:20:15 -0800

    knex

---
+++ seeds/pet_type.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+// seeds/pet_type.js
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('pet_type').del()
+    .then( () => {
+      // Inserts seed entries
+      return knex('pet_type').insert([
+        {id: 1, name: 'Bird'},
+        {id: 2, name: 'Cat'},
+        {id: 3, name: 'Dog'},
+        {id: 4, name: 'Fish'},
+      ]);
+    });
+};
--- src/app.js
+++ src/app.js
@@ -2,3 +2,22 @@ const express = require('express');
 const app = express();
 const port = 8081; 
 
+const knex = require('knex')(require('path/to/knexfile.js')[process.env.NODE_ENV||'development']);
+
+app.use(express.json());
+
+app.get('/pets', function(req, res) {
+  knex('pet')
+    .select('*')
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'The data you are looking for could not be found. Please try again'
+      })
+    );
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT}`);
+});
